099a9373df518a6c0fd525db29fdf35819ec1471
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-04-30 21:04:07 +0600

    Session management added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 "use client";
-import { Button, ButtonGroup } from "@heroui/react";
+import { Avatar, Button, ButtonGroup } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png"
+import { authClient } from "@/lib/auth-client";
+import { MdOutlineLogout } from "react-icons/md";
 
 const Navbar = () => {
+  const { data: session, isPending, } = authClient.useSession()
+  const userData = session?.user;
+  console.log(userData);
+
+
   return (
     <div className="border-b px-2 py-3">
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
@@ -17,7 +24,7 @@ const Navbar = () => {
             height={30}
             className="object-cover h-auto w-auto"
           />
-          <h3 className="font-black text-lg">pixgen.</h3>
+          <h3 className="font-black text-lg">pixgen</h3>
         </div>
 
         <ul className="flex items-center gap-5 text-sm">
@@ -34,16 +41,24 @@ const Navbar = () => {
             <Link href={"/profile"}>Profile</Link>
           </li>
         </ul>
-
-        <ButtonGroup className="">
-          <Button className="flex items-center  text-sm">
-              <Link href={"/signup"}>SignUp</Link>
-          </Button>
-          <Button variant="secondary">
-
-              <Link href={"/signin"}>SignIn</Link>
-          </Button>
-        </ButtonGroup>
+        {isPending ? "..." :
+          userData ? <div className="flex justify-between items-center gap-4">
+            <Avatar>
+              <Avatar.Image alt="John Doe" src="https://img.heroui.chat/image/avatar?w=400&h=400&u=3" />
+              <Avatar.Fallback>JD</Avatar.Fallback>
+            </Avatar>
+            <h2 className="text-sm">Hello, {userData?.name}</h2>
+            <Button variant="danger" ><MdOutlineLogout />Logout</Button>
+          </div> :
+            <ButtonGroup className="">
+              <Button className="flex items-center  text-sm">
+                <Link href={"/signup"}>SignUp</Link>
+              </Button>
+              <Button variant="secondary">
+                <Link href={"/signin"}>SignIn</Link>
+              </Button>
+            </ButtonGroup>
+        }
       </nav>
     </div>
   );
